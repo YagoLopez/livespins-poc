47237eb6923495aa57c051b2be03eb59bc563142
Yago López <yago.lopez@gmail.com>
2024-05-03 00:50:48 +0200

    feat: game list scroll to top

---
+++ components/icons/LeftArrowIcon.tsx
@@ -0,0 +1,13 @@
+const UpArrowIcon = () => (
+  <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+    <g id="Layer_2">
+      <path d="M13,26a1,1,0,0,1-.71-.29l-9-9a1,1,0,0,1,0-1.42l9-9a1,1,0,1,1,1.42,1.42L5.41,16l8.3,8.29a1,1,0,0,1,0,1.42A1,1,0,0,1,13,26Z" />
+      <path d="M28,17H4a1,1,0,0,1,0-2H28a1,1,0,0,1,0,2Z" />
+    </g>
+    <g id="frame">
+      <rect className="cls-1" height="32" width="32" />
+    </g>
+  </svg>
+)
+
+export default UpArrowIcon
