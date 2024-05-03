ee1c4cde6b5b647e5f30923a71c10d04902853f2
Yago López <yago.lopez@gmail.com>
2024-05-03 18:52:44 +0200

    UI improvements (#4)
    * chore: update vulnerable deps and loader
    
    * feat: game list scroll to top
    
    * feat: add left arrow icon and fade-in style
    
    * feat: improve button styles

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader() {
   return (
     <div className="flex justify-center w-full text-white absolute mt-[1rem] mb-[1rem]">
-      Loading...
+      <div className="bg-gray-100 text-gray-500 px-6 rounded">Loading...</div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import { IGame } from "./api/lib/interfaces/IGame"
 import { InfiniteData } from "@tanstack/query-core"
 import HeadContent from "../components/HeadContent"
 import Logo from "../components/Logo"
+import UpArrowIcon from "../components/icons/UpArrowIcon"
 
 const Lobby: NextPage = ({ search }: Record<string, string>) => {
   const inputRef = useRef<HTMLInputElement>(null)
@@ -24,6 +25,7 @@ const Lobby: NextPage = ({ search }: Record<string, string>) => {
   const gamesRepository = new GamesRepository()
   const { useSearchPaginated } = useRepository(gamesRepository)
   const dispatch = useAppDispatch()
+  const [gameListScroll, setGameListScroll] = useState(0)
 
   const getInitialData = () =>
     scrolledGames?.pages?.length > 0 ? scrolledGames : undefined
@@ -33,6 +35,11 @@ const Lobby: NextPage = ({ search }: Record<string, string>) => {
       selectedGame.id
     ) as HTMLDivElement
     selectedGameDiv?.scrollIntoView()
+  }
+
+  const onClickUpArrowIcon = () => {
+    gameListRef.current?.scrollTo(0, 0)
+    window.scrollTo(0, 0)
   }
 
   const {
@@ -74,37 +81,52 @@ const Lobby: NextPage = ({ search }: Record<string, string>) => {
     }
   }, [hasNextPage, fetchNextPage])
 
+  gameListRef.current?.addEventListener("scroll", () => {
+    setGameListScroll(gameListRef.current?.scrollTop || 0)
+  })
+
   return (
-    <div className="bg-black-200 h-[100vh]">
-      <HeadContent />
+    <>
+      <div className="bg-black-200 h-[100vh]">
+        <HeadContent />
 
-      <Logo classes="flex justify-center md:w-[1190px] md:mx-auto md:block" />
+        <Logo classes="flex justify-center md:w-[1190px] md:mx-auto md:block" />
 
-      <InputSearch
-        ref={inputRef}
-        inputSearchState={inputSearchState}
-        setInputSearchState={setInputSearchState}
-        gameListRef={gameListRef}
-        classes="flex justify-center md:w-[1190px] md:mx-auto md:block"
-      />
+        <InputSearch
+          ref={inputRef}
+          inputSearchState={inputSearchState}
+          setInputSearchState={setInputSearchState}
+          gameListRef={gameListRef}
+          classes="flex justify-center md:w-[1190px] md:mx-auto md:block"
+        />
 
-      {isError && <Error error={error} />}
+        {isError && <Error error={error} />}
 
-      {isFetching && <Loader />}
+        {isFetching && <Loader />}
 
-      <GameList
-        ref={gameListRef}
-        gameList={gameList}
-        inputSearchState={inputSearchState}
-        loadMoreBtnRef={loadMoreBtnRef}
-        hasNextPage={hasNextPage}
-        fetchNextPage={fetchNextPage}
-        isFetchingNextPage={isFetchingNextPage}
-        classes="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8
+        <GameList
+          ref={gameListRef}
+          gameList={gameList}
+          inputSearchState={inputSearchState}
+          loadMoreBtnRef={loadMoreBtnRef}
+          hasNextPage={hasNextPage}
+          fetchNextPage={fetchNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+          classes="fade-in grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8
                       md:gap-[40px] md:w-[1190px] h-[771px] overflow-y-auto
                       overflow-x-hidden m-auto mt-[56px]"
-      />
-    </div>
+        />
+      </div>
+      {gameListScroll > 10 && (
+        <div
+          className="fav-btn"
+          title="Scroll to top"
+          onClick={onClickUpArrowIcon}
+        >
+          <UpArrowIcon />
+        </div>
+      )}
+    </>
   )
 }
 
