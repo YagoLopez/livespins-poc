0755d4d7c9dc2040664d9da7873f0cd4bc59a034
Yago López <yago.lopez@gmail.com>
2024-05-08 18:50:14 +0200

    refactor: loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,9 @@
 export default function Loader() {
   return (
     <div className="flex justify-center w-full text-white absolute mt-[1rem] mb-[1rem]">
-      <div className="bg-gray-100 text-gray-500 px-6 rounded">Loading...</div>
+      <div className="bg-red-500 text-white border-2 font-medium px-6 rounded">
+        Loading...
+      </div>
     </div>
   )
 }
